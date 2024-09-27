3a78b8cf51c16915a125e620efa0b9e49e241d25
JFrost_L <133841544+JFrost-L@users.noreply.github.com>
2024-09-27 20:29:18 +0900

    Delete 07_00. Ex directory

--- 07_00. Ex/ex01. IntentFilter/MyData.kt
+++
@@ -1,8 +0,0 @@
-package com.frost.ex01intentfilter
-
-import android.graphics.drawable.Drawable
-
-data class MyData(var appLabel:String,
-                  var appClass:String,
-                  var appPackage: String,
-                  var appIcon:Drawable)
